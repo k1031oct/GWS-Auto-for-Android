b55a3f0e7991a7004ec021bbd49721cde8170be7
PathForest <k1031oct2000@gmail.com>
2025-11-16 20:06:26 +0900

    chore: Bump version code and name
    - Increment `versionCode` to 343
    - Update `versionName` to "1.2.343"
    - Remove `android:testOnly="true"` from the debug manifest

--- app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.gws.auto.mobile.android";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 342;
-  public static final String VERSION_NAME = "1.2.342";
+  public static final int VERSION_CODE = 343;
+  public static final String VERSION_NAME = "1.2.343";
 }
