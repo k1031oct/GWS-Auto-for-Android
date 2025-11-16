8b5fa1921c4440b01677e02f499bd399daae676f
PathForest <k1031oct2000@gmail.com>
2025-11-16 10:53:26 +0900

    feat: Bump version and add chart color
    - Increment `VERSION_CODE` to 322 and `VERSION_NAME` to "1.2.322"
    - Add `chart_red` color resource for use in charts

--- app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.gws.auto.mobile.android";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 321;
-  public static final String VERSION_NAME = "1.2.321";
+  public static final int VERSION_CODE = 322;
+  public static final String VERSION_NAME = "1.2.322";
 }
