ddc0124e793cc4129b37e79dfec7eb2d3927d0dc
PathForest <k1031oct2000@gmail.com>
2025-11-17 02:24:08 +0900

    chore: Bump version code and name
    - Increment `versionCode` to 355
    - Update `versionName` to "1.2.355"

--- app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.gws.auto.mobile.android";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 354;
-  public static final String VERSION_NAME = "1.2.354";
+  public static final int VERSION_CODE = 355;
+  public static final String VERSION_NAME = "1.2.355";
 }
