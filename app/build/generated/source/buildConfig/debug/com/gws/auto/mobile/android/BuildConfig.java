98c032babba08f11b714f6be7a33029e4411c159
PathForest <k1031oct2000@gmail.com>
2025-11-23 16:24:18 +0900

    feat: Implement drag-and-drop for workflow items
    - Add drag-and-drop functionality to the workflow item rows
    - Introduce `DraggableWorkflowItemRow` composable
    - Define drag sources and drop targets for reordering items

--- app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/gws/auto/mobile/android/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.gws.auto.mobile.android";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 418;
-  public static final String VERSION_NAME = "1.2.418";
+  public static final int VERSION_CODE = 419;
+  public static final String VERSION_NAME = "1.2.419";
 }
